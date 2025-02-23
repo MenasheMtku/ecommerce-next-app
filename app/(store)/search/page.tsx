3893d05a85989cfde5c4351d711a70bc1592b9f9
MenasheMtku <menashemetuko@gmail.com>
2025-02-23 07:25:34 +0200

    first commit

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+async function SearchPage({
+  searchParams,
+}: {
+  searchParams: {
+    query: string;
+  };
+}) {
+  const { query } = searchParams;
+  return <div>SearchPage for {query}</div>;
+}
+
+export default SearchPage;
